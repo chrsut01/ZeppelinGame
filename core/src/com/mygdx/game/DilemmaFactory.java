d30d1954ca1529872028378a6bd7210a84e4bdf7
chrsut01 <chrissu360@gmail.com>
2024-04-18 09:47:00 +0200

    manual json handling. DilemmaFactory etc

--- core/src/com/mygdx/game/DilemmaFactory.java
+++ core/src/com/mygdx/game/DilemmaFactory.java
@@ -2,15 +2,23 @@ package com.mygdx.game;
 
 //import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DilemmaFactory {
+    private AssetManager assetManager;
+    private JsonValue dilemmaData;
 
-    public static Dilemma createDilemma(String question, List<String> answers, String imagePath, String consequence) {
+    public DilemmaFactory() {
+        assetManager = new AssetManager();
+    }
+
+    public Dilemma createDilemma(String question, List<String> answers, String imagePath, String consequence) {
         Dilemma dilemma = new Dilemma();
-        // Set attributes based on parameters
         dilemma.setQuestion(question);
         dilemma.setAnswers(answers);
         dilemma.setImagePath(imagePath);
@@ -19,12 +27,33 @@ public class DilemmaFactory {
     }
     private List<Dilemma> dilemmas;
 
-    public List<Dilemma> loadDilemmasFromJson(String jsonFilePath) {
-        // Load dilemmas from JSON file
-        Json json = new Json();
-        List<Dilemma> dilemmas = json.fromJson(List.class, Dilemma.class, Gdx.files.internal(jsonFilePath));
+    public List<Dilemma> loadDilemmasFromJson(String jsonFileName) {
+        List<Dilemma> dilemmas = new ArrayList<>();
+        assetManager.load(jsonFileName, JsonValue.class);
+        assetManager.finishLoading();
+        JsonValue jsonValue = assetManager.get(jsonFileName, JsonValue.class);
+
+        for (JsonValue dilemmaData : jsonValue) {
+            String question = dilemmaData.getString("question");
+            List<String> answers = new ArrayList<>();
+            for (JsonValue answerData : dilemmaData.get("answers")) {
+                answers.add(answerData.asString());
+            }
+            String imagePath = dilemmaData.getString("imagePath");
+            String consequence = dilemmaData.getString("consequence");
+
+            Dilemma dilemma = createDilemma(question, answers, imagePath, consequence);
+            dilemmas.add(dilemma);
+        }
         return dilemmas;
     }
+
+  /*  public void loadDilemma(String dilemmaFileName) {
+        assetManager.load("json/" + dilemmaFileName, JsonValue.class);
+        assetManager.finishLoading();
+        dilemmaData = assetManager.get("json/" + dilemmaFileName, JsonValue.class);
+    }*/
+
 
     public Dilemma getDilemma(int index) {
         return dilemmas.get(index);
