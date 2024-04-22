a9051f61022609c8cf297c8646b02c89b8c90766
chrsut01 <chrissu360@gmail.com>
2024-04-22 17:43:57 +0200

    ZeppelinGame altered, but still "this.gameLevel is null"

--- core/src/com/mygdx/game/ZeppelinGame.java
+++ core/src/com/mygdx/game/ZeppelinGame.java
@@ -8,6 +8,7 @@ import com.mygdx.game.Screens.ClosingScreen;
 import com.mygdx.game.Screens.DilemmaScreen;
 import com.mygdx.game.Screens.SideScrollerScreen;
 import com.mygdx.game.Screens.IntroScreen;
+import com.mygdx.game.SideScrollers.SideScrollerBulg;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,16 +16,18 @@ import java.util.List;
 
 public class ZeppelinGame extends Game {
 
-    private List<GameLevel> levels;
+    private GameLevel currentLevel;
+    private List<GameLevel> gameLevels;
     public SpriteBatch batch;
     public BitmapFont font;
     private IntroScreen introScreen;
     private SideScrollerScreen sideScrollerScreen;
     private ClosingScreen closingScreen;
     private DilemmaScreen dilemmaScreen;
-    public int playerProgress;
+
+    public int playerProgress = 0;
     private int currentLevelIndex;
-    public SideScrollerScreen SideScrollerBulg;
+    //public SideScrollerScreen SideScrollerBulg;
     private List<SideScrollerScreen> sideScrollers;
     private OrthographicCamera camera;
 
@@ -34,23 +37,23 @@ public class ZeppelinGame extends Game {
     @Override
     public void create() {
         camera = new OrthographicCamera();
-        levels = new ArrayList<>();
-       // GameLevel level1 = new GameLevel(new SideScrollerBulg(), someListOfDilemmas);
-
-        // GameLevel level1 = new GameLevel(/* parameters for level 1 */);
-        levels.add(new GameLevel(SideScrollerBulg, new ArrayList<>()));
+        gameLevels = new ArrayList<>();
 
         introScreen = new IntroScreen(this);
         dilemmaScreen = new DilemmaScreen(this);
-        sideScrollerScreen = new SideScrollerScreen();
         closingScreen = new ClosingScreen(this);
+
+        SideScrollerScreen sideScrollerBulg = new SideScrollerBulg();
+        currentLevel = new GameLevel(sideScrollerBulg, new ArrayList<>());
+        gameLevels.add(currentLevel);
 
         // Set the initial screen based on player progress
         if (playerProgress == 0) {
             setScreen(introScreen); // Show intro screen if game hasn't started yet
-        } else if (playerProgress < levels.size()) {
+        } else if (playerProgress < gameLevels.size()) {
             // Show the appropriate game screen if there are levels to play
             startSideScroller();
+            playerProgress ++;
         } else {
             setScreen(closingScreen); // Show closing screen if all levels are completed
         }
@@ -73,15 +76,15 @@ public class ZeppelinGame extends Game {
 
     public GameLevel getCurrentLevel() {
         // Ensure that playerProgress does not exceed the bounds of the levels list
-        int index = Math.min(playerProgress, levels.size() - 1);
-        return levels.get(index);
+        int index = Math.min(playerProgress, gameLevels.size() - 1);
+        return gameLevels.get(index);
     }
 
      // Method for progressing to the next level
     public void progressToNextLevel() {
         playerProgress++;
-        if (playerProgress >= levels.size()) {
-            playerProgress = levels.size() - 1;
+        if (playerProgress >= gameLevels.size()) {
+            playerProgress = gameLevels.size() - 1;
         }
 
         // Set the current screen to the game screen with the next level
